e54e06c4b2aa8edfa46cfc148d45efad3f05c078
mhambe <ianmhambe@gmail.com>
2025-12-16 14:25:45 +0300

    Test1

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -9,7 +9,7 @@ const AuthLayout = () => {
           <div className="text-center mb-8">
             <h1 className="text-4xl font-bold">
               <span className="text-primary-600">Ian</span>
-              <span className="text-secondary-600">Malavi</span>
+              <span className="text-secondary-600">Mhambe</span>
             </h1>
             <p className="text-gray-600 mt-2">Healthcare Appointment Scheduler</p>
           </div>
